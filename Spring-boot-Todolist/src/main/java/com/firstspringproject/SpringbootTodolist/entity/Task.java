cf920a337c5cf09d546b8e381c0720cd6cef430b
mohamed <mem000aza@gmail.com>
2022-09-13 01:14:25 +0200

    Using MySql server

--- Spring-boot-Todolist/src/main/java/com/firstspringproject/SpringbootTodolist/entity/Task.java
+++ Spring-boot-Todolist/src/main/java/com/firstspringproject/SpringbootTodolist/entity/Task.java
@@ -1,8 +1,5 @@
 package com.firstspringproject.SpringbootTodolist.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
@@ -10,9 +7,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Task {
     @Id
     private Long taskId;
@@ -25,6 +19,47 @@ public class Task {
     private boolean Completed ;
 
 
+    public Task(Long taskId, String message, boolean completed) {
+        this.taskId = taskId;
+        Message = message;
+        Completed = completed;
+    }
 
+    public Task( )
+    {
 
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "taskId=" + taskId +
+                ", Message='" + Message + '\'' +
+                ", Completed=" + Completed +
+                '}';
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
+
+    public boolean isCompleted() {
+        return Completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        Completed = completed;
+    }
 }
